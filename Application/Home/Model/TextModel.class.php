b2e5a8c925df0efa7e2bfc6fa8944a2e594a87a9
shaoxianhui <huihui6007@163.com>
2014-09-26 21:31:29 +0800

    增加消息缓存

--- Application/Home/Model/TextModel.class.php
+++ Application/Home/Model/TextModel.class.php
@@ -3,8 +3,11 @@ namespace Home\Model;
 use Think\Model;
 class TextModel extends Model {
     public function getText($id) {
+        if(S('textMessage_'.$id))
+            return S('textMessage_'.$id);
         $text = $this->where('id='.$id)->find();
         if($text == true) {
+            S('textMessage_'.$id, $text['message'], 3600);
             return $text['message'];
         }
     }
