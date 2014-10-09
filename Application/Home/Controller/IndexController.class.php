45db1e415108a5436ad1ee9207d6e25e49312ebe
shaoxianhui <huihui6007@163.com>
2014-10-09 23:53:24 +0800

    完善公司主页，图片上传，公司主页包括index，contact两个页面

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,4 +2,7 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+    public function feedback() {
+        $this->success('感谢您的支持！', U('Home/Index/index'));
+    }
 }
