d9be8ae8de70e9fd2ba858c710327bfeff4b575d
shaoxianhui <huihui6007@163.com>
2014-10-20 22:31:40 +0800

    改变Menu表结构，增加url字段，使后台能创建view菜单

--- Application/Wechat/Model/MenuModel.class.php
+++ Application/Wechat/Model/MenuModel.class.php
@@ -12,13 +12,21 @@ class MenuModel extends Model {
                 $sub_menus = $this->where('upperId='.$tm['id'])->order('sort')->select();
                 $c2 = 0;
                 foreach($sub_menus as $sm) {
-                    $sub_menus_array[$c2] = array('type' => $sm['type'], 'name' => $sm['name'], 'key' => $sm['key']);
+                    if($sm['type'] == 'click') {
+                        $sub_menus_array[$c2] = array('type' => $sm['type'], 'name' => $sm['name'], 'key' => $sm['key']);
+                    } elseif ($sm['type'] == 'view') {
+                        $sub_menus_array[$c2] = array('type' => $sm['type'], 'name' => $sm['name'], 'url' => $sm['url']);
+                    }
                     $c2 = $c2 + 1;
                 }
                 $menus_array[$c1] = array('name' => $tm['name'], 'sub_button' => $sub_menus_array);
             }
             else {
-                $menus_array[$c1] = array('type' => $tm['type'], 'name' => $tm['name'], 'key' => $tm['key']);
+                if($tm['type'] == 'click') {
+                    $menus_array[$c1] = array('type' => $tm['type'], 'name' => $tm['name'], 'key' => $tm['key']);
+                } elseif ($tm['type'] == 'view') {
+                    $menus_array[$c1] = array('type' => $tm['type'], 'name' => $tm['name'], 'url' => $tm['url']);
+                }
             }
             $c1 = $c1 + 1;
         }
