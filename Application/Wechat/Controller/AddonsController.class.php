48a10697c3cf58f8ef3f99a12b86c040194a4bc3
shaoxianhui <huihui6007@163.com>
2014-10-25 22:00:39 +0800

    测试：大转盘在移动设备上是否流畅

--- Application/Wechat/Controller/AddonsController.class.php
+++ Application/Wechat/Controller/AddonsController.class.php
@@ -9,4 +9,11 @@ class AddonsController extends Controller {
     public function dzp() {
         $this->display();
     }
+
+    public function dzp_data() {
+        $data['success'] = true;
+        $data['prizetype'] = 2;
+        $data['sn'] = 'sn';
+        $this->ajaxReturn($data);
+    }
 }
