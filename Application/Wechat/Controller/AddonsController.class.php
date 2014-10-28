ed97286632b0e9fa6f8f13757d8bbee3349608fa
shaoxianhui <huihui6007@163.com>
2014-10-28 22:21:56 +0800

    大转盘和刮刮卡通过OAuth授权认证访问

--- Application/Wechat/Controller/AddonsController.class.php
+++ Application/Wechat/Controller/AddonsController.class.php
@@ -2,12 +2,21 @@
 namespace Wechat\Controller;
 use Think\Controller;
 class AddonsController extends Controller {
+    public function oAuth() {
+        $weObj = new \Org\Wechat\Wechat(C('WECHAT'));
+        $access_token = $weObj->getOauthAccessToken();
+        if($access_token) {
+            $this->assign('openId', $access_token['openid']);
+            $this->display($_GET['state']);
+        } else {
+            die('no access!');
+        }
+        
+    }
     public function ggk() {
-        $this->display();
     }
 
     public function dzp() {
-        $this->display();
     }
 
     public function dzp_data() {
