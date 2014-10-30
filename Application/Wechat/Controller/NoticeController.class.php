d52f233f5b8f4c6a55a23ec670118d7bce432123
shaoxianhui <huihui6007@163.com>
2014-10-30 23:38:37 +0800

    增加企业公告功能

---
+++ Application/Wechat/Controller/NoticeController.class.php
@@ -0,0 +1,63 @@
+<?php
+namespace Wechat\Controller;
+use Think\Controller;
+use Think\Log;
+class NoticeController extends Controller {
+    private $weObj;
+    public function __construct()
+    {
+        parent::__construct();
+        $options = array(
+            'token'=>'meirixianguo',
+            'appid'=>'wx0f3313fd2a9eed6e',
+            'appsecret'=>'5WpAyyPu4RtWtZ6qufgnBuaAGvZJ26wwgJuy0k7fYAaYDyR8X0UHEabkPw-H0ugV',
+ 			'encodingaeskey'=>'dkPjxDa2pNi9xYa1dbakWn7kMxXajQxF9rDteEevP83',
+ 			'agentid'=>'4',
+        );
+        $this->weObj = new \Org\Wechat\QyWechat($options);
+    }
+
+    public function index() {
+        $this->weObj->valid();
+        $type = $this->weObj->getRev()->getRevType();
+        switch($type) {
+        case \Org\Wechat\Wechat::MSGTYPE_TEXT:
+            $message = array( "touser" => "@all",
+                "safe" => "0",
+                "agentid" => "4",
+                "msgtype" => "text",
+                "text" => array(
+                    "content" => $this->weObj->getRevContent(),
+            ));
+            $this->weObj->sendMessage($message);
+            $this->weObj->text('公告已发布！！')->reply();
+            break;
+        case \Org\Wechat\Wechat::MSGTYPE_EVENT:
+            $event = $this->weObj->getRevEvent();
+            if($event !== false) {
+                switch(strtolower($event['event'])) {
+                case 'subscribe':
+                    break;
+                case 'unsubscribe':
+                    break;
+                case 'scan':
+                    break;
+                case 'click':
+                    break;
+                case 'location':
+                    break;
+                default:
+                }
+            }
+            break;
+        case \Org\Wechat\Wechat::MSGTYPE_IMAGE:
+            $this->weObj->text('hello world!')->reply();
+            break;
+        default:
+            $this->weObj->text('hello world!')->reply();
+        }
+    }
+
+    public function test($userId = 'ljl') {
+    }
+}
