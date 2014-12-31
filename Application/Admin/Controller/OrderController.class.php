fec93391e009af28d0e2396b72692eb2389f929f
shaoxianhui <huihui6007@163.com>
2014-12-31 17:05:28 +0800

    实现订单以及订单项的录入编辑操作

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -16,4 +16,41 @@ class OrderController extends Controller {
         $data['data'] = $orders;
         $this->ajaxReturn($data);
     }
+
+    public function crud($action = 'none', $data = null, $id = null) {
+        $result['fieldErrors'] = array();
+        switch($action) {
+        case 'create':
+        case 'edit':
+            if(strlen($data['customer']) <= 0)
+                array_push($result['fieldErrors'], array('name' => 'customer', 'status' => '客户名称不能为空'));
+            if(!empty($result['fieldErrors'])) {
+                $this->ajaxReturn($result);
+            }
+            break;
+        default:
+            break;
+        }
+        switch($action) {
+        case 'edit':
+            D('Order')->where('id='.$id)->save($data);
+            $data['id'] = $id;
+            $result['row'] = $data;
+            $this->ajaxReturn($result);
+            break;
+        case 'create':
+            $id = D('Order')->add($data);
+            $data['id'] = $id;
+            $result['row'] = $data;
+            $this->ajaxReturn($result);
+            break;
+        case 'remove':
+            D('Order')->relation(true)->delete(join(',', $id));
+            $result['id'] = $id;
+            $this->ajaxReturn($result);
+            break;
+        default:
+            break;
+        }
+    }
 }
--- Application/Admin/Controller/OrderItemController.class.php
+++ Application/Admin/Controller/OrderItemController.class.php
@@ -15,4 +15,48 @@ class OrderItemController extends Controller {
         $data['data'] = $orders;
         $this->ajaxReturn($data);
     }
+
+    public function crud($orderId, $action = 'none', $data = null, $id = null) {
+        $result['fieldErrors'] = array();
+        switch($action) {
+        case 'create':
+        case 'edit':
+            if(strlen($data['product']) <= 0)
+                array_push($result['fieldErrors'], array('name' => 'product', 'status' => '产品名称不能为空'));
+            if(!is_numeric($data['quantity'])) {
+                array_push($result['fieldErrors'], array('name' => 'quantity', 'status' => '数量不能为非数字'));
+            }
+            if(!is_numeric($data['money'])) {
+                array_push($result['fieldErrors'], array('name' => 'money', 'status' => '金额不能为非数字'));
+            }
+            if(!empty($result['fieldErrors'])) {
+                $this->ajaxReturn($result);
+            }
+            break;
+        default:
+            break;
+        }
+        switch($action) {
+        case 'edit':
+            D('OrderItem')->where('id='.$id)->save($data);
+            $data['id'] = $id;
+            $result['row'] = $data;
+            $this->ajaxReturn($result);
+            break;
+        case 'create':
+            $data['orderId'] = $orderId;
+            $id = D('OrderItem')->add($data);
+            $data['id'] = $id;
+            $result['row'] = $data;
+            $this->ajaxReturn($result);
+            break;
+        case 'remove':
+            D('OrderItem')->delete(join(',', $id));
+            $result['id'] = $id;
+            $this->ajaxReturn($result);
+            break;
+        default:
+            break;
+        }
+    }
 }
