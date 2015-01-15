2662daef04bfb010ceb84c4af3f8c32904bae71b
shaoxianhui <huihui6007@163.com>
2015-01-16 00:07:37 +0800

    完善支付功能，创建内部代金券

--- Application/Wechat/Controller/WxpayController.class.php
+++ Application/Wechat/Controller/WxpayController.class.php
@@ -9,19 +9,47 @@ class WxpayController extends Controller {
 			$url = $pay->createOauthUrlForCode("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
             Header("Location: $url");
 		} else {
+            // 换取openId
 			$openId = $pay->getOpenId($code);
 
+            // 填充已知数据
             $pay->setParameter("openid","$openId");
             $pay->setParameter("body","办公室水果预约");
             $timeStamp = time();
             $out_trade_no = C('WECHAT.appid').substr($openId, -4)."$timeStamp";
             $pay->setParameter("out_trade_no","$out_trade_no"); 
-            $pay->setParameter("total_fee","1200");
             $pay->setParameter("notify_url","http://$_SERVER[HTTP_HOST]/wechat/wxpay/notify");
             $pay->setParameter("trade_type","JSAPI");
-            $pay->getPrepayId();
-            $jsApiParameters = $pay->getParameters();
 
+            // 计算价格
+            $moneys = array(25, 20, 15, 10, 5, 25, 25);
+            $money = $moneys[date('w')] * 100;
+
+            // 查找代金券
+            $where['openId'] = $openId;
+            $where['valid'] = 1;
+            $user_voucher = M('UserVoucher')->where($where)->order('ctime asc')->find();
+            if($user_voucher != null) {
+                $voucher = M('Voucher')->find($user_voucher['voucherId']);
+                $pay->setParameter("attach", $user_voucher['id']);
+                $money -= $voucher['money'];
+                if($money <= 0) {
+                    $money = 1;
+                }
+                $this->assign('money', ($money / 100).'元(使用了一张代金券)');
+            } else {
+                $this->assign('money', ($money / 100).'元');
+            }
+            $pay->setParameter("total_fee","$money");
+
+            // 获得支付ID
+            $pay->getPrepayId();
+
+            // 生成package
+            $jsApiParameters = $pay->getParameters();
+            $this->assign('jsApiParameters', $jsApiParameters);
+
+            // 查找用户手机号，名字，地点
             $map['openId'] = $openId;
             $order = M('UserOrder')->where($map)->order('ctime desc')->find();
             if($order != null) {
@@ -38,18 +66,14 @@ class WxpayController extends Controller {
                     }
                 }
             }
-
-            $moneys = array(25, 20, 15, 10, 5, 25, 25);
-            $this->assign('money', $moneys[date('w')]);
-
             $this->assign('openId', $openId);
             $this->assign('trade', $out_trade_no);
 
+            // 填充地点信息
             $locations = M('Location')->where('valid=1')->select();
             $this->assign('locations', $locations);
 
-            $this->assign('jsApiParameters', $jsApiParameters);
-
+            // 支持JSSDK的验证签名
             $jssdk = new \Org\Wechat\JSSDK(C('WECHAT.appid'), C('WECHAT.appsecret'));
             $signPackage = $jssdk->GetSignPackage();
             $this->assign('signPackage', $signPackage);
@@ -61,6 +85,7 @@ class WxpayController extends Controller {
     public function notify() {
 		$notify = new \Org\Wechat\WxPay();
         $xml = $GLOBALS['HTTP_RAW_POST_DATA'];	
+        Log::write($xml, Log::INFO);
         $notify->saveData($xml);
         
         if($notify->checkSign() == FALSE){
@@ -73,6 +98,7 @@ class WxpayController extends Controller {
         $data = $notify->getData();
         if($data['return_code'] == 'SUCCESS') {
             if($data['result_code'] == 'SUCCESS') {
+                // 确认收款
                 $map['trade'] = $data['out_trade_no'];
                 $order = M('UserOrder')->where($map)->find();
                 if($order != null) {
@@ -94,6 +120,10 @@ class WxpayController extends Controller {
                     $message['data']['time'] = array('value' => $order['cdate'], 'color' => '#000000');
                     $message['data']['remark'] = array('value' => '记得及时取回水果', 'color' => '#000000');
                     $weObj->sendTemplateMessage($message);
+                }
+                // 销毁代金券
+                if(isset($data['attach'])) {
+                    M('UserVoucher')->where('id='.$data['attach'])->setField('valid', 0);
                 }
             }
         }
@@ -128,5 +158,35 @@ class WxpayController extends Controller {
         $this->assign('money', $moneys[date('w')]);
         $this->assign('openId', $openid);
         $this->display();
+    }
+
+    public function getVoucher($openId = null, $voucherId = 1) {
+        $voucher = M('Voucher')->find($voucherId);
+        if($voucher == null) {
+            $result['success'] = false;
+            $result['message'] = '代金券获得已下线！';
+            $this->ajaxReturn($result);
+        } else {
+            $map['openId'] = $openId;
+            $map['voucherId'] = $voucherId;
+            $count = M('UserVoucher')->where($map)->count();
+            if($count < $voucher['ulimit']) {
+                $data['openId'] = $openId;
+                $data['voucherId'] = $voucherId;
+                $data['valid'] = 1;
+                $data['ctime'] = time();
+                M('UserVoucher')->data($data)->add();
+                $result['success'] = true;
+                $result['message'] = $voucher['description'];
+                $this->ajaxReturn($result);
+
+            } else {
+                $result['success'] = false;
+                $result['message'] = '您已达到代金券的上线！';
+                $this->ajaxReturn($result);
+
+            }
+        }
+
     }
 }
